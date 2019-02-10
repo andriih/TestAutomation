73dbd999fbc514790b57fe2f6821cff324e2c200
andrii.hnatyshyn <andrii.hnatyshyn@gmail.com>
2019-02-10 17:30:49 +0200

    Unit 2 added

---
+++ TestAutomation_Unit2/TestAutomation_Unit2/Person.cs
@@ -0,0 +1,39 @@
+﻿namespace TestAutomation_Unit2
+{
+    public class Person
+    {
+        private string bDay;
+        private int age;
+        private string gender;
+        private int height;
+        private string info;
+
+        public Person(string birthDay)
+        {
+            bDay = birthDay;
+        }
+
+        private int getAge()
+        {
+            return age = 20;
+        }
+
+        private string getGender()
+        {
+            return gender = "male";
+        }
+
+        private int getHeight()
+        {
+            return height = 170;
+        }
+
+        public string GetInfo()
+        {
+             return  info = "Birth day: "+bDay+"\nAge: "+getAge()+"\nGender: "+getGender()+"\nHeight: "+getHeight();
+        }
+
+
+
+    }
+}
---
+++ TestAutomation_Unit2/TestAutomation_Unit2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAutomation_Unit2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("12/12/1990");
+
+            Console.WriteLine(person.GetInfo());
+            Console.ReadKey();
+        }
+    }
+}
