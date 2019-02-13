7f981b3fdd952031ac87ada4e54a6ab41f4b0508
Andrii Hnat <andrii.hnatyshyn@gmail.com>
2019-02-13 18:02:18 +0200

    Types: init

--- TestAutomation_Unit2/TestAutomation_Unit2/Program.cs
+++ TestAutomation_Unit2/TestAutomation_Unit2/Program.cs
@@ -16,8 +16,13 @@ namespace TestAutomation_Unit2
             Person girl = new Person("02/14/1992","female",165);
 
             //Console.WriteLine(boy.GetInfo());
-            Console.WriteLine(girl.GetInfo());
+            //Console.WriteLine(girl.GetInfo());
+
+            Types type = new Types();
+            type.getConvertedStr("1 2 3");
+            Console.WriteLine(type.calc(12, "/", 45));
             Console.ReadKey();
+
         }
     }
 }
---
+++ TestAutomation_Unit2/TestAutomation_Unit2/Types.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAutomation_Unit2
+{
+    class Types
+    {
+        private string str;
+
+        public void getConvertedStr(string str)
+        {
+            string[] words = str.Split(' ');
+            int sum = 0;
+
+            foreach (var word in words)
+            {
+                int result = Convert.ToInt32(word);
+
+                System.Console.WriteLine($"<{result}>");
+
+                sum += result;
+            }
+
+            System.Console.WriteLine(sum);
+        }
+
+        public int calc(int firstNum, string operation, int secondNum)
+        {
+           int result;
+
+            switch (operation)
+            {
+                case "+":
+                    return result = firstNum + secondNum;
+                case "-":
+                    return result = firstNum - secondNum;
+                case "*":
+                    return result = firstNum * secondNum;
+                case "/":
+                    return result = firstNum / secondNum;
+                default:
+                    return result = 0;
+            }
+
+        }
+    }
+}
