1ef6062d2bd0ddabc9f7dcb7e2dc10126ba6a3cc
andrii.hnatyshyn <andrii.hnatyshyn@gmail.com>
2019-03-06 23:54:30 +0200

    Unit5: Mode/Median/Mean added

--- TestAutomation_Unit2/TestAutomation_Unit2/Program.cs
+++ TestAutomation_Unit2/TestAutomation_Unit2/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections; 
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,14 +11,35 @@ namespace TestAutomation_Unit2
     {
         static void Main(string[] args)
         {
-            List<int> intList = new List<int>() { 5, 17, 3, 9, 14, 2};
+            List<int> intList = new List<int>() { 2, 3, 3, 5, 7 , 10 };
             Statement numbers = new Statement();
-            Console.WriteLine(numbers.getMeanInt(intList));
+            Console.WriteLine("Mean: "+numbers.getMean(intList));
+            Console.WriteLine("Median: "+numbers.getMedian(intList));
+            Console.WriteLine("Mode: "+numbers.getMode(intList));
 
-            Console.WriteLine(numbers.getMedianInt(intList));
+            List<string> names = new List<string>()
+            {
+               "Danae",
+               "James",
+               "Rima",
+               "Aida",
+               "Coretta",
+               "Jeff",
+               "Adam",
+               "Alvaro",
+               "Kristal",
+               "Marisa",
+               "Jackqueline"
+            };
 
-            numbers.getMedianInt2(intList);
+            Statement name = new Statement();
+            name.SearchForParticularNames(names);
 
+            ArrayList list = new ArrayList() { "string", 11.34, "text", 77.0001, "Some text", 23 };
+            Statement intAndString = new Statement();
+            intAndString.ConvertToInt(list);
+
+            Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
         }
     }
--- TestAutomation_Unit2/TestAutomation_Unit2/Statement.cs
+++ TestAutomation_Unit2/TestAutomation_Unit2/Statement.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,9 +11,9 @@ namespace TestAutomation_Unit2
     {
         private List<int> collection { get; set; }
 
-        public int getMeanInt(List<int> collection)
+        public int getMean(List<int> collection)
         {
-            int mean = 0;
+            int mean = default(int);
             foreach (int number in collection)
             {
                 mean += number;
@@ -20,20 +21,96 @@ namespace TestAutomation_Unit2
             return mean / collection.Count;
         }
 
-        public  int getMedianInt(List<int> collection)
+        public  int getMedian(List<int> collection)
         {
-            collection.Sort();
-            return (collection[0] + collection[(collection.Count) / 2]) / 2;
+            int[] array = collection.ToArray();
+            Array.Sort(array);
+            for (int i = 0, j = array.Length - 1; i < j; i++, j--)
+            {
+                int t = array[i];
+                array[i] = array[j];
+                array[j] = t;
+            }
+            int count = array.Count();
+            int first = array[count / 2];
+            int second = array[(count / 2) - 1 ];
+
+            if (count%2 == 0)
+            {
+                return (first + second) / 2;
+            }
+
+            return array[(count / 2) - 1];
         }
 
-        public void getMedianInt2(List<int> collection)
+        public int getMode(List<int> collection)
         {
-            List<int> newList = new List<int>();
-            newList.Add(collection.Sort());
-            Console.WriteLine("=======================");
-            Console.WriteLine(collection[0]);
-            Console.WriteLine(collection[(collection.Count) / 2]);
+            int[] array = collection.ToArray();
+            Dictionary<int, int> counts = new Dictionary<int, int>();
+            foreach (int a in array)
+            {
+                if (counts.ContainsKey(a))
+                    counts[a] = counts[a] + 1;
+                else
+                    counts[a] = 1;
+            }
+
+            int result = int.MinValue;
+            int max = int.MinValue;
+            foreach (int key in counts.Keys)
+            {
+                if (counts[key] > max)
+                {
+                    max = counts[key];
+                    result = key;
+                }
+            }
+            return result;
         }
 
+       public void SearchForParticularNames(List<string> names)
+       {
+            Random rnd = new Random();
+            string[] arrayNames = names.ToArray();
+            Array.Sort(arrayNames);
+
+            int count = arrayNames.Length;
+            var last = count - 1;
+
+            for (var i = 0; i < last; ++i)
+            {
+                var r = rnd.Next(i, count);
+                var tmp = arrayNames[i];
+                arrayNames[i] = arrayNames[r];
+                arrayNames[r] = tmp;
+            }
+
+            foreach (string name in arrayNames)
+            {
+                if (name == "Adam")
+                {
+                    Console.WriteLine(" 'Adam' name was found!");
+                    continue;
+                }
+
+                if (name == "James")
+                {
+                    Console.WriteLine(" 'James' name was found!");
+                    break;
+                }
+            }
+       }
+        
+        public void ConvertToInt(ArrayList list)
+        {
+            foreach (string item in list)
+            {
+                if (Int32.TryParse(item, out int result))
+                {
+                   
+                }
+            }
+        }
     }
 }
+
