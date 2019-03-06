4e64ebdd5ac6b4b8bd8466cbd2982e6827ae3c7c
andrii.hnatyshyn <andrii.hnatyshyn@gmail.com>
2019-03-06 08:05:15 +0200

    Unit5: init

---
+++ TestAutomation_Unit2/TestAutomation_Unit2/Statement.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAutomation_Unit2
+{
+    class Statement
+    {
+        private List<int> collection { get; set; }
+
+        public int getMeanInt(List<int> collection)
+        {
+            int mean = 0;
+            foreach (int number in collection)
+            {
+                mean += number;
+            }
+            return mean / collection.Count;
+        }
+
+        public int getMedianInt(List<int> collection)
+        {
+            int mean = 0;
+            foreach (int number in collection)
+            {
+                mean += number;
+            }
+            return mean / collection.Count;
+        }
+    }
+}
