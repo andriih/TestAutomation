bd134df9d44f939e928b13fba995d8e3980942af
andrii.hnatyshyn <andrii.hnatyshyn@gmail.com>
2019-03-10 23:36:21 +0200

    Unit6: class added

---
+++ TestAutomation_Unit2/TestAutomation_Unit2/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAutomation_Unit2
+{
+    class Book : Item
+    {
+        public int Price { get; set; }
+        public string Name { get; set; }
+
+        public Book(string name, int price)
+        {
+            Price = price;
+            Name = name;
+        }
+
+        public override void Info()
+        {
+            Console.WriteLine("Name of book is '{0}', price: ${1}", Name, Price);
+        }
+    }
+}
--- TestAutomation_Unit2/TestAutomation_Unit2/Program.cs
+++ TestAutomation_Unit2/TestAutomation_Unit2/Program.cs
@@ -11,33 +11,8 @@ namespace TestAutomation_Unit2
     {
         static void Main(string[] args)
         {
-            List<int> intList = new List<int>() { 2, 3, 3, 5, 7 , 10 };
-            Statement numbers = new Statement();
-            Console.WriteLine("Mean: "+numbers.GetMean(intList));
-            Console.WriteLine("Median: "+numbers.GetMedian(intList));
-            Console.WriteLine("Mode: "+numbers.GetMode(intList));
-
-            List<string> names = new List<string>()
-            {
-               "Danae",
-               "James",
-               "Rima",
-               "Aida",
-               "Coretta",
-               "Jeff",
-               "Adam",
-               "Alvaro",
-               "Kristal",
-               "Marisa",
-               "Jackqueline"
-            };
-
-            Statement name = new Statement();
-            name.SearchForParticularNames(names);
-
-            ArrayList list = new ArrayList() { 23.44,"Ann", 34, "Dog", "Aplle 34" };
-            Statement intAndString = new Statement();
-            intAndString.ConvertToInt(list);
+            Book someBook = new Book("Some book",99);
+            someBook.Info();
 
             Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
